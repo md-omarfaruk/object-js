66fe755d4f4b37eb8a006d27a117a0f56ab20b08
md-omarfaruk <87226097+md-omarfaruk@users.noreply.github.com>
2021-10-28 09:02:29 +0600

    object
    object js

---
+++ object.js
@@ -0,0 +1,46 @@
+var student = { id: 3, phone: 0162, name: "Omar" };
+var teacher = { id: 7, phone: 0162, name: "Faruk"};
+
+    console.log(student);
+    console.log(teacher);
+
+
+var student = { id: 3, phone: 0162, name: "Omar"};
+var teacher = { id: 7, phone: 0162, name: "Faruk"};
+
+var phoneNo = student.phone;
+var NameStudent = student["name"];
+
+var idNo = "id";
+var idStudent = student[idNo];
+
+    console.log(idStudent);
+    console.log(phoneNo);
+    console.log(NameStudent);
+
+
+var friends = { id: 5, phone: 0156, name: "Jashim"};
+    console.log(friends);
+
+friends.id = 9;
+    console.log(friends);
+
+friends["phone"] = 0172;
+    console.log(friends);
+
+jashimName = "name";
+friends[jashimName] = "Pranto";
+    console.log(friends);
+
+
+var myFriends = { name1: "Jashim", name2: "Hossain", name3: "Hassan" }
+
+myFriends.name4 = "Habib";
+myFriends["name5"] = "Mazharul";
+    console.log(myFriends);
+
+
+
+
+
+
